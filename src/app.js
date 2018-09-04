e8e32b6b44e41bb7e27285816970e9306ab2d984
Riley Criddle <rileycriddle220@gmail.com>
2018-09-04 22:02:32 +0800

    feat(app): added logging on express to pac

--- src/app.js
+++ src/app.js
@@ -1,9 +1,28 @@
-import "./stylesheets/main.css";
+import { ipcRenderer } from 'electron';
+import './stylesheets/main.css';
 
 // Small helpers you might want to keep
-import "./helpers/context_menu.js";
-import "./helpers/external_links.js";
+import './helpers/context_menu.js';
+import './helpers/external_links.js';
 
-import Webview from "./renderers/webview";
+// import Webview from "./renderers/webview";
+//
+// new Webview("http://concretego.com/");
 
-new Webview("http://concretego.com/");
+const logsEl = document.getElementById('logs');
+
+function addLog(title, message, status = 'normal') {
+  const log = document.createElement('div');
+  log.setAttribute('class', `log-item-${status}`);
+  log.innerHTML = `<b>${title}</b> ${message}`;
+  logsEl.appendChild(log);
+
+  // stick scroll to bottom
+  if (logsEl.scrollTop >= logsEl.scrollHeight - 50) {
+    logsEl.scrollTop = logsEl.scrollHeight;
+  }
+}
+
+ipcRenderer.on('proxy-log', (event, args) => {
+  addLog(args.title, args.message, args.status);
+});
